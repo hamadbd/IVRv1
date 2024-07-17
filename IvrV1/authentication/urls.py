d1bd4dbbd7ca3d1b4230e0524f905b213ad22698
hamadbd <hamaddwairi0@gmail.com>
2024-07-17 14:56:06 +0300

    Audio Retrieveal Implementation

--- IvrV1/authentication/urls.py
+++ IvrV1/authentication/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RegisterView, AuthenticateView, RecordingPage, SuccessPage
+from .views import RegisterView, AuthenticateView, RecordingPage, SuccessPage, RecordingListView, LoginView, HomePageView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('authenticate/', AuthenticateView.as_view(), name='authenticate'),
+    path('ivr_call/', AuthenticateView.as_view(), name='ivr_call'),
     path('recording_page/', RecordingPage.as_view(), name='recording_page'),
     path('upload_recording/', RecordingPage.as_view(), name='upload_recording'),
     path('success/', SuccessPage.as_view(), name='success_page'),
+    path('recordings/', RecordingListView.as_view(), name='recording_list'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
